5a94a0e04fb3f1a795bc910f3fff0d5534fcf173
RektPunk <rektpunk@gmail.com>
2022-10-21 00:20:21 +0900

    feat - init commit

--- module/credential.py
+++ module/credential.py
@@ -5,12 +5,12 @@ from enum import Enum
 try:
     load_dotenv()
 except Exception as e:
-    raise("Credential load failed!")
+    raise ("Credential load failed!")
 
 
 class Tokens(str, Enum):
-    SLACK_APP_TOKEN: str= os.environ["SLACK_APP_TOKEN"]
-    SLACK_BOT_TOKEN: str= os.environ["SLACK_BOT_TOKEN"]
+    SLACK_APP_TOKEN: str = os.environ["SLACK_APP_TOKEN"]
+    SLACK_BOT_TOKEN: str = os.environ["SLACK_BOT_TOKEN"]
 
     def __repr__(self):
         return f"{self.value}"
