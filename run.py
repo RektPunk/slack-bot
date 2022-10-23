18a03cea4eb4b9ab2fc05f5d32f8aa1314ac4471
RektPunk <rektpunk@gmail.com>
2022-10-24 00:24:28 +0900

    feat - add ask introduction

--- run.py
+++ run.py
@@ -6,12 +6,18 @@ from variables.channel import ChannelId
 app = App(token=Tokens.SLACK_BOT_TOKEN)
 
 
+@app.event("team_join")
+def ask_for_introduction(event, say):
+    _channel_id = ChannelId.GENERAL
+    _user_id = event["user"]
+    _text = f"Welcome <@{_user_id}>! 🎉 You can introduce yourself in this channel."
+    say(text=_text, channel=_channel_id)
+
+
 @app.event("app_mention")
-def greetings(event, client, message, say):
-    print("event:", event)
-    print("client:", client)
-    print("message:", message)
-    say(f'hello! <@{event["user"]}>')
+def greetings(event, say):
+    _user_id = event["user"]
+    say(f"Hello! <@{_user_id}>")
 
 
 if __name__ == "__main__":
